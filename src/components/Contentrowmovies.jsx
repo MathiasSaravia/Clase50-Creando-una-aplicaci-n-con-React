fbcfc4df9a3d6bd715f00a6b95734a7944613aff
MathiasSaravia <maycolbeach@gmail.com>
2024-06-10 03:49:35 -0300

    clase51 tp terminado

--- src/components/Contentrowmovies.jsx
+++ src/components/Contentrowmovies.jsx
@@ -1,60 +1,21 @@
 import React from 'react'
+import { ContentData } from './ContentData'
 
-export const Contentrowmovies = () => {
+export const Contentrowmovies = ({data}) => {
+
   return (
-    <div class="row">
+    <div className="row">
  
-    {/* <!-- Movies in Data Base --> */}
-    <div class="col-md-4 mb-4">
-      <div class="card border-left-primary shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">21</div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-film fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+  {/* <!-- Movies in Data Base --> */}
+  {/*  <!-- Total awards --> */}
+  {/* <!-- Actors quantity --> */}
+  {
+  data.filter((el) => el.show)
+  .map((el,i) => {
+      return <ContentData key={i} title={el.title} color={el.color} number={el.numbre} icon={el.icon} />
+  })
+  }
 
-   {/*  <!-- Total awards --> */}
-    <div class="col-md-4 mb-4">
-      <div class="card border-left-success shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">79</div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-award fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    {/* <!-- Actors quantity --> */}
-    <div class="col-md-4 mb-4">
-      <div class="card border-left-warning shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
-              </div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">49</div>
-            </div>
-            <div class="col-auto">
-              <i class="fas fa-user fa-2x text-gray-300"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
   </div>
   )
 }
--- src/components/Contentrowtop.jsx
+++ src/components/Contentrowtop.jsx
@@ -4,19 +4,41 @@ import { Contentlastmovie } from './Contentlastmovie'
 import { Contentgenres } from './Contentgenres'
 
 export const Contentrowtop = () => {
+  const metrics = [{
+    show:true,
+    title:"Movies in Data Base",
+    color:"primary",
+    numbre:21,
+    icon:"film"
+  },
+  {
+    show:true,
+    title:"Total awards",
+    color:"success",
+    numbre:79,
+    icon:"award"
+  },
+  {
+    show:true,
+    title:"Actors quantity",
+    color:"warning",
+    numbre:49,
+    icon:"user"
+  }]
   return (
-       <div class="container-fluid">
-       <div class="d-sm-flex align-items-center justify-content-between mb-4">
-         <h1 class="h3 mb-0 text-gray-800">App Dashboard</h1>
+
+       <div className="container-fluid">
+       <div className="d-sm-flex align-items-center justify-content-between mb-4">
+         <h1 className="h3 mb-0 text-gray-800">App Dashboard</h1>
        </div>
      
        {/* <!-- Content Row Movies--> */}
-      <Contentrowmovies/>
+      <Contentrowmovies data={metrics}/>
      {/*   <!-- End movies in Data Base --> */}
        
  
        {/* <!-- Content Row Last Movie in Data Base --> */}
-       <div class="row">
+       <div className="row">
          {/* <!-- Last Movie in DB --> */}
        <Contentlastmovie/>
         {/*  <!-- End content row last movie in Data Base --> */}
